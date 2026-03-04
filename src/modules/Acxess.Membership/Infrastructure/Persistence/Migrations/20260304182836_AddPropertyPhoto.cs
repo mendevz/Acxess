ac837c08e54aa53c5cf059d1841640532195471c
mendev <mauromendezme@gmail.com>
2026-03-04 12:29:11 -0600

    feat: migration photoUrl

---
+++ src/modules/Acxess.Membership/Infrastructure/Persistence/Migrations/20260304182836_AddPropertyPhoto.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Acxess.Membership.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPropertyPhoto : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PhotoUrl",
+                schema: "Membership",
+                table: "Members",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PhotoUrl",
+                schema: "Membership",
+                table: "Members");
+        }
+    }
+}
