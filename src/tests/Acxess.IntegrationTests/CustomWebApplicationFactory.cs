62719648b3c52baeca6a36c32fb79adc756d146f
Mauro Mendez <mauromendezme@gmail.com>
2026-05-20 11:55:20 -0600

    feat(tests): create first member tests

---
+++ src/tests/Acxess.IntegrationTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,48 @@
+﻿using Acxess.Shared.Abstractions;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Testcontainers.MsSql;
+
+namespace Acxess.IntegrationTests;
+
+public class CustomWebApplicationFactory : WebApplicationFactory<Program>, IAsyncLifetime
+{
+    private readonly MsSqlContainer _dbContainer;
+
+    public CustomWebApplicationFactory()
+    {
+        _dbContainer = new MsSqlBuilder("mcr.microsoft.com/mssql/server:2022-latest")
+            .WithPassword("Acxess_Test_Pass_123!") 
+            .Build();
+    }
+
+    protected override void ConfigureWebHost(IWebHostBuilder builder)
+    {
+        builder.ConfigureAppConfiguration((context, config) =>
+        {
+            config.AddInMemoryCollection(new Dictionary<string, string?>
+            {
+                ["ConnectionStrings:Default"] = _dbContainer.GetConnectionString()
+            });
+        });
+
+        builder.UseEnvironment("Localhost");
+    }
+    public async Task InitializeAsync()
+    {
+        await _dbContainer.StartAsync();
+
+        using var scope = Services.CreateScope();
+        foreach (var seeder in scope.ServiceProvider.GetServices<IDataSeeder>())
+        {
+            await seeder.SeedAsync();
+        }
+    }
+
+    async Task IAsyncLifetime.DisposeAsync()
+    {
+        await _dbContainer.DisposeAsync();
+    }
+}
---
+++ src/tests/Acxess.IntegrationTests/MemberTests.cs
@@ -0,0 +1,36 @@
+﻿
+using Acxess.Membership.Domain.Entities;
+using Acxess.Membership.Infrastructure.Persistence;
+using FluentAssertions;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Acxess.IntegrationTests;
+
+public class MemberTests(CustomWebApplicationFactory factory) : IClassFixture<CustomWebApplicationFactory>
+{
+    [Fact]
+    public async Task Should_Saver_Member_In_Database()
+    {
+        // Arrange 
+        using var scope = factory.Services.CreateAsyncScope();
+        var dbContext = scope.ServiceProvider.GetRequiredService<MembershipModuleContext>();
+
+        var nuevoSocio =  Member.Create(
+            tenantId: 1,
+            firstName: "Mauro",
+            lastName: "Mendez",
+            createdByUser: 1,
+            phone: "123456789",
+            email: "");
+
+        // Act 
+        dbContext.Members.Add(nuevoSocio);
+        await dbContext.SaveChangesAsync();
+
+        // Assert
+        var socioGuardado = await dbContext.Members.FindAsync(nuevoSocio.IdMember);
+
+        socioGuardado.Should().NotBeNull();
+        socioGuardado!.FirstName.Should().Be("Mauro");
+    }
+}
